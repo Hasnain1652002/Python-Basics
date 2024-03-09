cc0ffc4cf50e21220a454825c9309fa764251f58
Hasnain <112103324+Hasnain1652002@users.noreply.github.com>
2024-03-09 23:34:04 +0500

    Add files via upload

---
+++ word order.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import Counter
+n = int(input())
+l = [ ]
+for i in range(n):
+    l.append(input())
+
+x = Counter(l)
+
+print ( len(x))
+print (*x.values())
