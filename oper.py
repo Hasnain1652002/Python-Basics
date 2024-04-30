9f539f91d2bc8de64d257d8b1765c028ad7b70d9
Hasnain <112103324+Hasnain1652002@users.noreply.github.com>
2024-04-30 15:10:58 +0500

    Add files via upload

---
+++ oper.py
@@ -0,0 +1,23 @@
+x = 6
+if (type(x)is int) :
+    print ("true")
+
+else:
+    print("f")
+
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9]
+for item in l1:
+    if item in l2:
+        print("op")
+else:
+    print("no")
+
+print("=====================")
+a=60
+b=13
+c=0
+c=a>>2
+print("line 1",c)
+
